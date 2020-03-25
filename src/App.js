9db097df8659b68732281dcac7a81ee7c45876f4
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-25 17:03:22 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(this.renderGetRequest)
+  }
+
+  renderGetRequest(crew) {
+    for (const member in crew) {
+      return (<div>Name: {member.name}, Craft: {member.craft}</div>)
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hello</h1>
+        {this.renderGetRequest()}
+      </div>
+    )
+  }
+}
+
+export default App
